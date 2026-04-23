a0d9563a769c75a0d04c22da0efa21d2ef23bee0
Isabella Oliveira <138396545+isabella3107@users.noreply.github.com>
2026-04-23 20:29:23 -0300

    modificação do consumidor
    Added KafkaProducer to send processed messages to a new topic.

--- consumer.py
+++ consumer.py
@@ -1,19 +1,38 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 from const import *
 import sys
 
-# Create consumer: Option 1 -- only consume new events
-consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+# Consumer (lendo do topic1)
+consumer = KafkaConsumer(
+    bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT]
+)
 
-# Create consumer: Option 2 -- consume old events (uncomment to test -- and comment Option 1 above)
-#consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
+# Producer (enviando para topic2)
+producer = KafkaProducer(
+    bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT]
+)
 
 try:
-  topic = sys.argv[1]
+    topic_in = sys.argv[1]   # topic1
+    topic_out = sys.argv[2]  # topic2
 except:
-  print ('Usage: python3 consumer <topic_name>')
-  exit(1)
-  
-consumer.subscribe([topic])
+    print('Usage: python3 consumer.py <topic_in> <topic_out>')
+    exit(1)
+
+consumer.subscribe([topic_in])
+
 for msg in consumer:
-    print (msg.value)
+    # 🔹 Mensagem original
+    original = msg.value.decode()
+    print("Received:", original)
+
+    # 🔹 Processamento (exemplo simples)
+    processed = original.upper()
+
+    print("Processed:", processed)
+
+    # 🔹 Envia para o novo tópico
+    producer.send(topic_out, value=processed.encode())
+
+# Garante envio
+producer.flush()
